f4b4742a7e61c482da17ba9ea48726a4c49a080a
Jevoy <jevoy.charvis@mymona.uwi.edu>
2018-11-12 19:29:00 +0000

    edit2

---
+++  JavaScript.js
@@ -0,0 +1,16 @@
+
+
+function myFunction(){
+     var btn = document.createElement("BUTTON");
+     btn.alert("p");
+     document.body.appendChild(btn);
+    
+}
+
+window.onload= function(){
+     myFunction()
+
+	
+
+	
+};
